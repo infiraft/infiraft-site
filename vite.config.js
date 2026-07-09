893e04f9c216d01e17b9cca62e4649560ad45e80
Infiraft <infiraft@gmail.com>
2026-07-09 13:35:23 +0700

    feat: add catering demo landing page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        katering: resolve(__dirname, 'demo/katering/index.html'),
+      },
+    },
+  },
   server: {
     host: '127.0.0.1',
     allowedHosts: true,
