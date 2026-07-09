4d6a07f8583a45a9091dd4397c0e5e1cb86d43fb
Infiraft <infiraft@gmail.com>
2026-07-09 12:14:57 +0700

    chore: use Cloudflare Pages static deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
-  plugins: [cloudflare()],
   server: {
     host: '127.0.0.1',
     allowedHosts: true,
